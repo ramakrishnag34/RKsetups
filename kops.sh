9c79c4895a7c0ddcb0ff4bfc8be124f3b8456e9a
ramakrishnag34 <116751336+ramakrishnag34@users.noreply.github.com>
2024-02-27 15:47:59 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -14,7 +14,7 @@ chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-aws s3api create-bucket --bucket rkccit987.k8s.local --region us-east-1
+aws s3api create-bucket --bucket rkccit987.k8s.local --region ap-south-2
 aws s3api put-bucket-versioning --bucket rkccit987.k8s.local --region ap-south-2 --versioning-configuration Status=Enabled
 export KOPS_STATE_STORE=s3://rkccit987.k8s.local
 kops create cluster --name rama.k8s.local --zones ap-south-2 --master-count=1 --master-size t3.medium --node-count=2 --node-size t3.micro

3a5286ea7d02fa0c30cf608788fe85fd09957d35
ramakrishnag34 <116751336+ramakrishnag34@users.noreply.github.com>
2024-02-27 15:11:50 +0530

    Create kops.sh

---
+++ kops.sh
@@ -0,0 +1,21 @@
+#vim .bashrc
+#export PATH=$PATH:/usr/local/bin/
+#source .bashrc
+
+
+#! /bin/bash
+apt update -y
+apt upgrade -y
+apt install awscli -y
+aws configure
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+wget https://github.com/kubernetes/kops/releases/download/v1.25.0/kops-linux-amd64
+chmod +x kops-linux-amd64 kubectl
+mv kubectl /usr/local/bin/kubectl
+mv kops-linux-amd64 /usr/local/bin/kops
+
+aws s3api create-bucket --bucket rkccit987.k8s.local --region ap-south-2
+aws s3api put-bucket-versioning --bucket rkccit987.k8s.local --region ap-south-2 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://rkccit987.k8s.local
+kops create cluster --name rama.k8s.local --zones ap-south-2 --master-count=1 --master-size t3.medium --node-count=2 --node-size t3.micro
+kops update cluster --name rama.k8s.local --yes --admin

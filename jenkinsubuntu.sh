3df6f366b4ccb4403169007f6e6f203481d59e87
ramakrishnag34 <116751336+ramakrishnag34@users.noreply.github.com>
2024-03-21 16:00:04 +0530

    Create jenkinsubuntu.sh

---
+++ jenkinsubuntu.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install default-jdk
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
